126b263bf20b2e6d737f63967755a78fac1349db
Takishima <Takishima@users.noreply.github.com>
2026-01-11 18:13:25 +0000

    deploy: becbf545e8eae1fccf59be5aa4a6c1a3c75d116e

---
+++ pr-716/ocr-poc/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/volleykit/pr-716/ocr-poc/sw.js', { scope: '/volleykit/pr-716/ocr-poc/' })})}
