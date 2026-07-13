7c626459c64981e1ab4febbbacd30e5800381746
Takishima <Takishima@users.noreply.github.com>
2026-07-13 04:11:51 +0000

    deploy: c0c813c264e2494e4abc93e06dccdb0242551f94

--- pr-1139/ocr-poc/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pr-1139/ocr-poc/sw.js', { scope: '/pr-1139/ocr-poc/' })})}
--- pr-1141/ocr-poc/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pr-1141/ocr-poc/sw.js', { scope: '/pr-1141/ocr-poc/' })})}
